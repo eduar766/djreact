321a662593e05733588eae24126348c71ac3d478
Eduardo S <eduar766@gmail.com>
2018-11-01 22:09:21 -0400

    Rest Framework Aplicated. Api

---
+++ backend/src/articles/api/views.py
@@ -0,0 +1,13 @@
+from .serializers import ArticleSerializers
+from articles.models import Article
+
+from rest_framework.generics import ListAPIView, RetrieveAPIView
+
+
+class ArticleListView(ListAPIView):
+    queryset = Article.objects.all()
+    serializer_class = ArticleSerializers
+
+class ArticleDetailView(RetrieveAPIView):
+    queryset = Article.objects.all()
+    serializer_class = ArticleSerializers
